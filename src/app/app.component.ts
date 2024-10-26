0f9bebe1eb819597446e531ad1bd633cca6bf3f7
Riya Bhavsar <rymbhavsar@gmail.com>
2024-10-26 19:12:50 +0530

    added analytics to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GoogleAnalyticsService } from './google-analytics.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-angular-project';
+  constructor(private googleAnalyticsService: GoogleAnalyticsService) {
+  }
+
+  ngOnInit() {
+    this.googleAnalyticsService.loadGoogleAnalytics()
+    }
+  
+    trackEvent(event: string) {
+      this.googleAnalyticsService.trackEvent(event, event); 
+    }
+    
 }
